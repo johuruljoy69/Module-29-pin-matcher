1fc9fd8f7c404bab991cd3da3f258618a371bf36
johirjoy <joy.sd69@gmail.com>
2023-02-17 19:53:52 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,66 @@
+function getPin(){
+    const pin = generatePin();
+    const pinString = pin + '';
+    if(pinString.length === 4){
+        return pin;
+    }
+    else{
+        // console.log('pin not 3 digit found', pin);
+        return getPin();
+    }
+}
+    // 
+    function generatePin(){
+        const random = Math.round(Math.random()*10000);
+        return random;
+    }
+    // 
+    document.getElementById('generate-pin').addEventListener('click', function(){
+        const pin = getPin();
+        
+        // display pin
+        const displayPinField = document.getElementById('display-pin');
+        displayPinField.value = pin;
+
+    })
+    document.getElementById('calculator').addEventListener('click', function(event){
+        const number = event.target.innerText;
+        const typedNumberField = document.getElementById('typed-numbers');
+        const previousTypeNumber = typedNumberField.value;
+        if(isNaN(number)){
+            if(number === 'C'){
+                typedNumberField.value = '';
+            }
+            else if(number === '<'){
+                const digit = previousTypeNumber.split('');
+                digit.pop();
+                const remainingDigit = digit.join('');
+                typedNumberField.value = remainingDigit;
+            }
+        }
+        else{
+            const newTypedNumber = previousTypeNumber + number;
+            typedNumberField.value = newTypedNumber;
+        }
+    })
+
+    document.getElementById('verify-pin').addEventListener('click', function(){
+        const displayPinField = document.getElementById('display-pin');
+        const currentPin = displayPinField.value;
+
+        const typedNumberField = document.getElementById('typed-numbers');
+        const typedNumber = typedNumberField.value;
+        // 
+        const pinSuccessMessage = document.getElementById('pin-success'); 
+        const pinFailureMessage = document.getElementById('pin-failure');
+
+        if(typedNumber === currentPin){
+            pinSuccessMessage.style.display = 'block';
+            pinFailureMessage.style.display = 'none';
+        }
+        else{
+            pinFailureMessage.style.display = 'block';
+            pinSuccessMessage.style.display = 'none';
+        }
+    })
+
